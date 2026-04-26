862328bf313df7e16d397b2153afcb66abb9c1a9
moemenhafez <moemen.hafez777@gmail.com>
2026-04-27 01:13:39 +0300

    added navbar components from bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+import Navbar from './navbar/navbar';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+// src/main.tsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+export default App;
